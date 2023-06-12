2361fd45c005da626f26ceca8d21b57754049a63
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2023-06-12 06:45:16 +0200

    ci: Add GitHub Actions

---
+++ .github/workflows/before-script-linux.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [ -f /usr/bin/yum ]; then
+    yum install -y openssl-devel pcsc-lite-devel centos-release-scl llvm-toolset-7
+    echo Before
+    clang --version || true
+    source /opt/rh/llvm-toolset-7/enable
+    echo After
+    clang --version || true
+
+    PREFIX=/
+    GMP_VERSION=6.2.1
+    NETTLE_VERSION=3.9.1
+
+ cd /tmp && \
+    curl -O https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.bz2 && \
+    tar xvf gmp-${GMP_VERSION}.tar.bz2 && \
+    cd gmp-${GMP_VERSION} && \
+    ./configure --prefix=$PREFIX && \
+    make && make install
+
+cd /tmp &&
+    curl -O https://ftp.gnu.org/gnu/nettle/nettle-${NETTLE_VERSION}.tar.gz && \
+    tar xvf nettle-${NETTLE_VERSION}.tar.gz && \
+    cd nettle-${NETTLE_VERSION} && \
+    ./configure --prefix=$PREFIX \
+      --with-lib-path=$PREFIX/lib \
+      --with-include-path=$PREFIX/include \
+      --disable-openssl && \
+    make && make install
+else
+    apt-get update
+    apt-get install -yqq cargo clang git nettle-dev pkg-config libssl-dev openssl libpcsclite-dev llvm
+fi
