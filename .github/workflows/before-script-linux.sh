12cc70c2f3ae3062d9e14c443e03e6ccbe14bd37
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2023-06-13 13:06:19 +0200

    Add GCC multilib
    See: https://stackoverflow.com/a/54082790

--- .github/workflows/before-script-linux.sh
+++ .github/workflows/before-script-linux.sh
@@ -27,5 +27,5 @@ cd /tmp &&
     make && make install
 else
     apt-get update
-    apt-get install -yqq cargo clang git nettle-dev pkg-config libssl-dev openssl libpcsclite-dev llvm
+    apt-get install -yqq cargo clang git nettle-dev pkg-config libssl-dev openssl libpcsclite-dev llvm gcc-multilib
 fi
